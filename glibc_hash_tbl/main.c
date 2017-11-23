5d91552c7520f752f06b43d5d2df8430f5e1ff91
Vitaly German <vgerman@vgerman-co5.workgroup>
2017-11-23 02:04:30 -0800

    Minor project fix

--- glibc_hash_tbl/main.c
+++ glibc_hash_tbl/main.c
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   main.c
- * Author: vgerman
- *
+ * Fixed version of https://gist.github.com/cirocosta/e61e52cc6afe2633500b
  * Created on November 22, 2017, 7:26 AM
  */
 #include <search.h>
